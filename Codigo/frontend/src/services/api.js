b631e427a9a676d27ec54f4192a332451c7463cc
Arthur Araújo <arthur.mendonca.1352200@sga.pucminas.br>
2025-12-05 17:23:48 -0300

    fix frontend

--- Codigo/frontend/src/services/api.js
+++ Codigo/frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  // ELE TENTA LER A VARIÁVEL DE AMBIENTE PRIMEIRO
+  // Se não encontrar (no ambiente local), ele usa o localhost como padrão.
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
 });
 
 export default api;
